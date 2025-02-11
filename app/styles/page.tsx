23b05667767d5974dab2dbbfb5a00409e40e74d9
PwnOwn <aloutsss@outlook.com>
2025-02-11 16:35:21 +0800

    Create page.tsx

---
+++ app/styles/page.tsx
@@ -0,0 +1,15 @@
+import Footer from "../components/footer";
+
+export default function Develop() {
+    return (
+      <div className="">
+        <header className="text-center">
+        <h1 className="text-xl font-bold">styles Page</h1>
+        <p className="">This page will help you get started with development resources and tutorials.</p>
+
+        </header>
+      </div>
+
+
+    )
+}
