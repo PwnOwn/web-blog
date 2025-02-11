37cf4c0e6a1df5ed6520a668ef47fd41020fcacc
PwnOwn <aloutsss@outlook.com>
2025-02-11 21:06:16 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Sidebar from "./components/sidebar";
 import Footer from "./components/footer";
 
 import { Inter } from 'next/font/google'; // 示例：使用 Google Fonts
+import Link from "next/link";
 
 const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
@@ -25,7 +26,16 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=optional"
         />
       </head>
-      <body className={inter.className}>
+
+      <body className={inter.className} id='main-content'> 
+              {/* 跳过链接，设置 focus 时可见 */}
+      <Link
+          href="#main-content"
+          className="absolute top-[-40px] left-0 px-4 py-2 bg-black text-white font-bold focus:top-0 focus:z-10 focus:block focus:outline-none focus:focus:ring-2 focus:focus:ring-blue-500"
+          aria-label="Skip to main content"
+        >
+          Skip to main content
+        </Link>
         <div className="flex min-h-screen flex-col bg-[#fdf7f9] dark:bg-[#141314]">
           <Sidebar />
           <main className="flex-1 ml-[86px]">{children}</main>
