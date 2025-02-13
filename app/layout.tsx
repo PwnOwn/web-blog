7dd96fe34a6d2cd741f88394f1ea42c1ebe61cba
PwnOwn <aloutsss@outlook.com>
2025-02-13 15:31:45 +0800

    change icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=optional"
         />
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+
+
       </head>
 
       <body className={inter.className} id='main-content'> 
